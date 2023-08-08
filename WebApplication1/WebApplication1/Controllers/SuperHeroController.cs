95c6eda15bb9cfeda632c73e205e337e7eb998ed
Ant <hark777@hotmail.com>
2023-08-08 15:07:06 +1000

    Add SuperHero controller and test data

---
+++ WebApplication1/WebApplication1/Controllers/SuperHeroController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return new List<SuperHero>() {
+                new SuperHero {Id=1, Name="Batman", FirstName="Danny", LastName="Thompson", Place="Testville"  },
+            new SuperHero {Id=2, Name="Robinhood", FirstName="Robin", LastName="Hood", Place="Forest"  }
+
+            };
+        }
+    }
+}
